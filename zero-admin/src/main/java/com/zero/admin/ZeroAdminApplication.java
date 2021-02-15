36197289d010c7aff562ea25ce5457f856e18e1e
Juffing <zhou_jun_feng@outlook.com>
2021-02-15 15:18:03 +0800

    第一次提交

---
+++ zero-admin/src/main/java/com/zero/admin/ZeroAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zero.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroAdminApplication.class, args);
+    }
+
+}
