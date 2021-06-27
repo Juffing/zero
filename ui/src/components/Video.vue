affad4d61f50bd074f52cd42f6a816bb64958a33
Juffing <zhou_jun_feng@outlook.com>
2021-06-28 00:14:29 +0800

    添加成功

--- ui/src/components/Video.vue
+++ ui/src/components/Video.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Video">
     <el-button type="text" @click="addCodeVisible = true">点击打开 Dialog</el-button>
-    <el-dialog title="提示" v-model="addCodeVisible" width="400px" :before-close="handleClose">
+    <el-dialog title="提示" v-model="addCodeVisible" width="400px" :before-close="handleClose" :close-on-click-modal="false">
       <div>
         <el-form ref="form" :model="addCodeForm" label-width="80px" size="mini">
           <el-form-item label="番号：">
@@ -14,7 +14,7 @@
             <el-input v-model="addCodeForm.descp"></el-input>
           </el-form-item>
           <el-form-item size="large">
-            <el-button>取消</el-button>
+            <el-button @click="handleClose">取消</el-button>
             <el-button type="primary" @click="onSubmit">提交</el-button>
           </el-form-item>
         </el-form>
@@ -39,6 +39,11 @@ export default {
   methods: {
     handleClose() {
       this.addCodeVisible = false;
+      this.addCodeForm = {
+        code: '',
+        name: '',
+        descp: '',
+      }
     },
     onSubmit() {
       this.$axios.post(this.$url + '/backend/code', {
@@ -47,6 +52,9 @@ export default {
         'descp':this.addCodeForm.descp
       })
       .then((response) => {
+        if (response.data.code == 200) {
+          this.addCodeVisible = false;
+        }
         console.log(response)
       })
     },
