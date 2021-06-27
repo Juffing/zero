a50b3191ce187a8c26e6cef7c081bc3f23f1fd84
Juffing <zhou_jun_feng@outlook.com>
2021-06-27 23:56:14 +0800

    编号添加成功

--- web/backend/src/main/java/com/zero/backend/controller/CodeController.java
+++ web/backend/src/main/java/com/zero/backend/controller/CodeController.java
@@ -4,12 +4,8 @@ import com.zero.backend.model.param.AddCodeParam;
 import com.zero.backend.service.CodeService;
 import com.zero.backend.utils.StringsUtils;
 import com.zero.result.ZeroResult;
-import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/backend")
@@ -20,11 +16,14 @@ public class CodeController {
     private CodeService codeService;
 
     @PostMapping("/code")
-    public ZeroResult addCode(AddCodeParam addCodeParam) {
+    public ZeroResult addCode(@RequestBody AddCodeParam addCodeParam) {
         if (StringsUtils.isEmpty(addCodeParam.getCode()) || StringsUtils.isEmpty(addCodeParam.getName())) {
             return ZeroResult.fail("参数错误");
         }
         Integer result = codeService.addCode(addCodeParam);
+        if (result == null) {
+            return ZeroResult.fail("番号重复");
+        }
         return ZeroResult.success(result);
     }
 
